be7afb6c22e2aaa29415559dc21e74162d52f168
leekelleher <leekelleher@gmail.com>
2019-07-19 15:13:42 +0100

    [WIP] Added BoxList editor
    I'm not 100% convinced about this yet, unsure how it
    differs from CheckboxList or ToggleGroup experiences.

---
+++ src/Umbraco.Community.Contentment/DataEditors/BoxList/BoxListDataListEditor.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright © 2019 Lee Kelleher, Umbrella Inc and other contributors.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using System.Collections.Generic;
+using Umbraco.Core.IO;
+
+namespace Umbraco.Community.Contentment.DataEditors
+{
+#if !DEBUG
+    // TODO: IsWorkInProgress - Under development.
+    [global::Umbraco.Core.Composing.HideFromTypeFinder]
+#endif
+    internal class BoxListDataListEditor : IDataListEditor
+    {
+        public string Name => "Box List";
+
+        public string Description => "Select values from a list of box options.";
+
+        public string Icon => "icon-brick";
+
+        public Dictionary<string, object> DefaultConfig => default;
+
+        public string View => IOHelper.ResolveUrl(Constants.Internals.EditorsPathRoot + "box-list.html");
+    }
+}
